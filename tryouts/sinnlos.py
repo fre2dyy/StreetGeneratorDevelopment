64ce0b288dbf0eb592812f33624424edbb0c41e4
Tobias Krueger <t.krueger@tu-ilmenau.de>
2018-01-09 11:18:55 +0100

    test commit

--- tryouts/sinnlos.py
+++ tryouts/sinnlos.py
@@ -1,15 +1,38 @@
-import shapefile
+# import shapefile
+#
+#
+# sf = shapefile.Reader("files/colour/streets.shp")
+# shapes = sf.shapes()
+# bbox = shapes[3].bbox
+#
+# print bbox
+# print len(shapes)
+# print sf.fields
+# print sf.records()
+#
+# s = sf.shape(0)
+# print s.__geo_interface__["type"]
+#
 
 
-sf = shapefile.Reader("files/colour/streets.shp")
-shapes = sf.shapes()
-bbox = shapes[3].bbox
 
-print bbox
-print len(shapes)
-print sf.fields
-print sf.records()
+# Prepare the environment
 
-s = sf.shape(0)
-print s.__geo_interface__["type"]
 
+
+import sys
+from qgis.core import *
+from PyQt4.QtGui import *
+app = QApplication([])
+QgsApplication.setPrefixPath("/usr", True)
+QgsApplication.initQgis()
+
+# Prepare processing framework
+sys.path.append('C:/OSGeo4W64/apps/qgis/python/plugins/processing') # Folder where Processing is located
+from processing.core.Processing import Processing
+Processing.initialize()
+from processing.tools import *
+# import processing
+# clipped_roads = processing.runalg('qgis:clip', osm_road_linetrstrings, your_shapefile, None)
+# clipped_roads_layer = QgsVectorLayer(clipped_roads['OUTPUT'], 'Clipped OSM Roads', 'ogr')
+# QgsMapLayerRegistry.instance().addMapLayer(clipped_roads_layer, False)
